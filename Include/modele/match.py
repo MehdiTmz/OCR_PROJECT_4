17c5227e663f8fe4d2255dd59560da41ff40fb95
Tabi Mehdi <mehditmz1@gmail.com>
2022-08-07 01:30:53 +0200

    match_result_created

--- Include/modele/match.py
+++ Include/modele/match.py
@@ -1,7 +1,25 @@
 class Match:
 
-    def __init__(self, match1, match2, state):
+    def __init__(self, player1, player2):
 
-        self.match1 = match1
-        self.match2 = match2
-        self.state = state
+        self.player1 = player1
+        self.player2 = player2
+
+    def result(self, state):
+
+        if (state == 1):
+
+            score1 = 1
+            score2 = 0
+
+        elif (state == 2):
+
+            score1 = 0
+            score2 = 1
+
+        else:
+
+            score1 = 0.5
+            score2 = 0.5
+
+        return ([self.player1, score1], [self.player2, score2])
